f29eeddbbe273b8f7c86843d8243646f00d3680e
Andreas <al17883@essex.ac.uk>
2020-09-22 10:13:13 +0100

    Created an R-project and script where all the coding will ocuur. Reading CSV files

---
+++ R-Script.R
@@ -0,0 +1,11 @@
+## R Code to underatke MT5763 Individual Project
+## GitHub Repository URL : https://github.com/AMLoucas/MT5763_1_200029834
+## GitHub Cloning URL : https://github.com/AMLoucas/MT5763_1_200029834.git
+
+## Importing libray tidyverse
+library(tidyverse)
+
+## Reading CSV files to apply computations on.
+seoul_Bikes <- read.csv("BikeSeoul.csv")
+washington_Bikes <- read.csv("BikeWashingtonDC.csv")
+
