4e9a422661d555482b497f9f5ade051611d9d203
Andreas <al17883@essex.ac.uk>
2020-09-22 10:56:35 +0100

    First 3 tasks od Data Wranglind seoul_Bikes done

--- R-Script.R
+++ R-Script.R
@@ -7,5 +7,34 @@ library(tidyverse)
 
 ## Reading CSV files to apply computations on.
 seoul_Bikes <- read.csv("BikeSeoul.csv")
+## Showing data read
+head(seoul_Bikes)
 washington_Bikes <- read.csv("BikeWashingtonDC.csv")
+## Showing data read
+head(washington_Bikes)
 
+## Data wrangling on file BikeSeoul
+## Task 1 Remove Rows that Functioning Day is No, after remove column.
+## Number of rows before Removing :
+nrow(seoul_Bikes)
+seoul_Bikes <- seoul_Bikes %>%
+  filter(Functioning.Day != "No") %>%
+  select(-Functioning.Day)
+## Number of rows after Removing Rows
+nrow(seoul_Bikes)
+## New Table after structure after removing Column Functioning.Day
+head(seoul_Bikes)
+
+## Task 2 Renaming Column Names Where needed [Date,Count,Hour,Temperature,WindSpeed,Season,Holidy]
+## Header Names before Renaming
+head(seoul_Bikes)
+seoul_Bikes <- seoul_Bikes %>%
+  rename(Count = Rented.Bike.Count, Temperature = Temperature.C., WindSpeed = Wind.speed..m.s., Season = Seasons)
+## Header Names after Renaming
+head(seoul_Bikes)
+
+## Task 3 changing column date type to Date.
+seoul_Bikes$Date <- as.Date(seoul_Bikes$Date)
+
+
+  
